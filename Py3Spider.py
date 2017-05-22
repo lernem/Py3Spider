8943f54644ff9def2d49941cc21476d184554cf3
lerne <lernem@foxmail.com>
2017-05-22 23:53:59 +0800

    Test zhihu.

--- Py3Spider.py
+++ Py3Spider.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 
 import requests
+import re
 
-r = requests.get('http://www.baidu.com')
-print(type(r))
-print(r.status_code)
-print(type(r.text))
-print(r.text)
-print(r.cookies)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/52.0.2743.116 Safari/537.36'
+}
+r = requests.get('https://www.zhihu.com/explore', headers=headers)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+titles = re.findall(pattern, r.text)
+print(titles)
